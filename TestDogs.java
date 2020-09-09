b5b161946b5edfb9182d3234fe33ec6b36ad41fd
arisestar <66844949+arisestar@users.noreply.github.com>
2020-09-09 11:26:11 -0400

    Add files via upload

---
+++ TestDogs.java
@@ -0,0 +1,22 @@
+// exercise pgs 202-205
+ // contuie on 270-272
+
+public class TestDogs
+{
+	public static void main(String[] args) 
+	{
+		// variables and constants 
+		DogTri dog1 = new DogTri("Bowser", 2, 85,89, 0);
+		dog1.display();
+		DogTri dog2 = new DogTri("Rush", 3, 78, 72, 80);
+		dog2.display();
+		DogTri dog3 = new DogTri("Ginger" , 3 ,90 ,86 ,72);
+		dog3.display();
+
+		// output phase 
+		dog1.display();
+		dog2.display();
+		dog3.display();
+
+	}
+}
